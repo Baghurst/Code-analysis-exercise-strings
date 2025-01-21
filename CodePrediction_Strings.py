d2d230e4bf9534ef15bcb638f84e541b65c60bf9
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-21 13:39:18 +0000

    Add files via upload

---
+++ CodePrediction_Strings.py
@@ -0,0 +1,52 @@
+#1
+print("Number 1: ")
+#A
+print("A:")
+
+print ("""
+1
+    2
+        3
+""" )
+#B
+print("B:")
+text = "Text.\nNext line."
+print (text)
+#C
+print("C:")
+print ('One', ' Two ' * 2)
+print ('One ' + 'Two' * 2)
+print (len('0123456789'))
+#D
+print("D:")
+print (", ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
