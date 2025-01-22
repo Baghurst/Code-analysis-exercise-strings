a33a44624c6456642c09e6fa164fe388c9795fcd
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-22 11:59:29 +0000

    Update CodePrediction_Strings.py

--- CodePrediction_Strings.py
+++ CodePrediction_Strings.py
@@ -18,8 +18,11 @@ print ('One', ' Two ' * 2)
 print ('One ' + 'Two' * 2)
 print (len('0123456789'))
 #D
-print("D:")
-print (", ")
+print("D: ")
+s = '0123456789'
+print(s[3], ", ", s[0 : 3],"-", s[2 : 5])
+print(s[:3], " - ", s[3:], " , ", s[3:100])
+print(s[20:], s[2:1], s[1:1])
 
 
 
